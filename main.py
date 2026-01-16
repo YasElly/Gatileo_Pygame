9880baade63cc23b607968015f97e3298379659f
Yasmim Emanuelly <yef.brasil@gmail.com>
2026-01-16 12:30:30 -0300

    Telas

--- main.py
+++ main.py
@@ -24,3 +24,18 @@ tutorial2 = pygame.image.load('gatileo/imagens/tutorial2.png')
 tutorial2 = tutorial2.convert()
 tutorial_loja = pygame.image.load('gatileo/imagens/tutorial_loja.png')
 tutorial_loja = tutorial_loja.convert()
+
+tela_comprou_granada = pygame.image.load("gatileo/imagens/comprou_granada.png")
+tela_comprou_granada = tela_comprou_granada.convert()
+
+tela_granada_inventario = pygame.image.load("gatileo/imagens/granada_inventario.png")
+tela_granada_inventario = tela_granada_inventario.convert()
+
+tela_comprou_viratempo = pygame.image.load("gatileo/imagens/comprou_viratempo.png")
+tela_comprou_viratempo = tela_comprou_viratempo.convert()
+
+tela_viratempo_inventario = pygame.image.load("gatileo/imagens/viratempo_inventario.png")
+tela_viratempo_inventario = tela_viratempo_inventario.convert()
+
+tela_dinheiro_insuficiente = pygame.image.load("gatileo/imagens/dinheiro_insuficiente.png")
+tela_dinheiro_insuficiente = tela_dinheiro_insuficiente.convert()
