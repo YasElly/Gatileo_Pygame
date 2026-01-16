486f4b5ccb2e366fcb7ff671f57a757642b86eaf
Yasmim Emanuelly <yef.brasil@gmail.com>
2026-01-16 00:42:23 -0300

    Inicio pt.2

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
 import pygame
-import sys
+
+pygame.init()
+pygame.display.set_caption('Gatíleo')
+largura = 640
+altura = 480
+font = pygame.font.Font(None, 24)
+screen = pygame.display.set_mode((largura, altura))
+clock = pygame.time.Clock()
+running = True
+
+titulo = pygame.image.load('gatileo/imagens/titulo2.png').convert_alpha()
+titulo = pygame.transform.smoothscale(titulo, (300, 96))
+
+tela_inicial = pygame.image.load('gatileo/imagens/img1.png')
+tela_inicial = tela_inicial.convert()  
